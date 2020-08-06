5f4f1160b58b302504fea354e3a2a8f5fed04ee4
leandromac <contato@leandromacedo.com>
2020-08-06 10:13:01 -0300

    fix Uncought SyntaxError

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const dummyTransactions = [
-  {id: 1, name: 'Bolo de banana', amount: -20},
-  {id: 2, name: 'Bolo de Laranja', amount: 30},
-  {id: 3, name: 'Bolo de Maçã', amount: 20},
-  {id: 4, name: 'Bolo de Chocolate', amount: 35}
+  { id: 1, name: 'Bolo de banana', amount: -20 },
+  { id: 2, name: 'Bolo de Laranja', amount: 30 },
+  { id: 3, name: 'Bolo de Maçã', amount: 20 },
+  { id: 4, name: 'Bolo de Chocolate', amount: 35 }
 ]
 
 const addTransactionIntoDOM = transaction => {
@@ -12,9 +12,7 @@ const addTransactionIntoDOM = transaction => {
   const li = document.createElement('li')
 
   li.classList.add(cssClass)
-  li.innerHTML = '
-    ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
-  '
+  li.innerHTML = '${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>'
   console.log(li)
 }
 
