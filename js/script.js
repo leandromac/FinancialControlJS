7e4df11293b0cceb2537095612f8c4247571aaef
leandromac <contato@leandromacedo.com>
2020-08-06 09:41:36 -0300

    Interpolation error innerHTML

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const dummyTransactions = [
+  {id: 1, name: 'Bolo de banana', amount: -20},
+  {id: 2, name: 'Bolo de Laranja', amount: 30},
+  {id: 3, name: 'Bolo de Maçã', amount: 20},
+  {id: 4, name: 'Bolo de Chocolate', amount: 35}
+]
+
+const addTransactionIntoDOM = transaction => {
+  const operator = transaction.amount < 0 ? '-' : '+'
+  const cssClass = transaction.amount < 0 ? 'minus' : 'plus'
+  const amountWithoutOperator = Math.abs(transaction.amount)
+  const li = document.createElement('li')
+
+  li.classList.add(cssClass)
+  li.innerHTML = '
+    ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+  '
+  console.log(li)
+}
+
+addTransactionIntoDOM(dummyTransactions[0])
